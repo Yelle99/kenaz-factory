d8bc54a4d7a7345c9ab1e32f0972c7f4768a3ba3
Yelle99 <jelena.hrga@gmail.com>
2021-05-24 12:06:27 +0200

    replace scss with sass

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,12 @@
   </div>
 </template>
 
-<style lang="scss">
-* {
-  margin: 0px;
-  padding: 0px;
-  box-sizing: border-box;
-}
+<style lang="sass">
+*
+  margin: 0px
+  padding: 0px
+  box-sizing: border-box
 
-body {
-  background-color: rgb(240, 240, 240);
-}
+body
+  background-color: rgb(240, 240, 240)
 </style>
